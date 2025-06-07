2054cc00163dbee5fa37fafdd3d297ac67b869ab
GH-robin <111631623+GH-robin@users.noreply.github.com>
2025-06-08 00:20:29 +0900

    create security group

--- main.tf
+++ main.tf
@@ -4,11 +4,54 @@ data "aws_ssm_parameter" "amazonlinux_2023" {
   name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-6.1-x86_64" # x86_64
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ssm_parameter.amazonlinux_2023.value
   instance_type = var.instance_type
 
+  vpc_security_group_ids = [aws_security_group.web.id]
+  
   tags = {
     Name = "HelloWorld"
   }
+}
+
+resource "aws_security_group" "web" {
+  name = "web"
+  description = "Allow http and https in. Allow everything out"
+
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "web_http_in" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.web.id
+}
+
+resource "aws_security_group_rule" "web_https_in" {
+  type = "ingress"
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.web.id
+}
+
+resource "aws_security_group_rule" "web_everything_out" {
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.web.id
 }
