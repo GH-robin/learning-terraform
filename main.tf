391f5fcc4a253ae09043d8586ce8a907d0109e5a
GH-robin <111631623+GH-robin@users.noreply.github.com>
2025-06-08 10:20:01 +0900

    change ami

--- main.tf
+++ main.tf
@@ -1,10 +1,25 @@
+data "aws_ami" "app_ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["979382823631"] # Bitnami
+}
 
 data "aws_vpc" "default" {
   default = true
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-06098fd00463352b6"
+  ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
   vpc_security_group_ids = [aws_security_group.web.id]
